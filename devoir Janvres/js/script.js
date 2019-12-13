3c2137971ea3804f37bd5dbd9cbae321e649567c
Marc-chv <48481303+Marc-chv@users.noreply.github.com>
2019-12-13 16:14:28 +0100

    Add files via upload

---
+++ devoir Janvres/js/script.js
@@ -0,0 +1,16 @@
+//side bar//
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "200px";
+    
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    
+}
+
+
+
+
+
+
